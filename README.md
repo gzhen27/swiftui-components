ae3993295b1c66db0dec9fceb37a9f8e9d7e5e42
Gray <grayzprogrammer@gmail.com>
2022-07-23 18:40:49 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftUI Components
+Creates reusable swiftUI components
+
---
+++ SwiftUIComponents/SwiftUIComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIComponentsApp.swift
+//  SwiftUIComponents
+//
+//  Created by G Zhen on 7/23/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
